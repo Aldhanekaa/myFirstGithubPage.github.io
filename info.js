984de5336aa51fd2ab9fa0d9d6ba6a018d78fc87
Aldhanekaa <67791514+Aldhanekaa@users.noreply.github.com>
2020-07-24 20:46:03 +0700

    Delete info.js

--- info.js
+++
@@ -1,8 +0,0 @@
-function popUp() {
-    swal({
-        title: "Warning !",
-        text: "This site is not supported for Mobile or iPad devices yet, bugs may occur",
-        icon: "warning",
-        button: true
-    })
-}

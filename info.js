e8a25328567449db2e3bd7cc5b2031af55a563df
Aldhanekaa <67791514+Aldhanekaa@users.noreply.github.com>
2020-07-24 20:48:07 +0700

    test1
    lol

---
+++ info.js
@@ -0,0 +1,8 @@
+function popUp() {
+    swal({
+        title: "Warning !",
+        text: "This site is not supported for Mobile or iPad devices yet, bugs may occur",
+        icon: "warning",
+        button: true
+    })
+}
